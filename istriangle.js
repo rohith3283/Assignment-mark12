4268fb9cf87a4b0a7d23a890fbbbca6ef71bfe69
rohith3283 <109816311+rohith3283@users.noreply.github.com>
2022-08-14 13:51:18 +0530

    updated quiz hypotenuse triangle

---
+++ istriangle.js
@@ -0,0 +1,24 @@
+const inputs = document.querySelectorAll('.angle-input');
+const isTriangleBtn = document.querySelector('#is-triangle-btn');
+const outputE1 = document.querySelector('#output');
+
+function calculateSumOfAngles(angle1, angle2, angle3) {
+    const sumOfAngles = angle1 + angle2 + angle3;
+
+    return sumOfAngles;
+}
+
+function isTriangle() {
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value),
+     Number(inputs[2].value));
+
+     if(sumOfAngles === 180) {
+        outputE1.innerText = "yay, The angles form triangle";
+     }
+     else {
+        outputE1.innerText = "oh oh! The angles don't form a triangle";
+     }
+    
+}
+
+isTriangleBtn.addEventListener("click", isTriangle)
